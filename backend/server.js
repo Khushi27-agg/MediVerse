a6af25622797c15ff8b12850f33fe7a119d8a281
Khushi-codes <khushiaggarwal2702@gmail.com>
2026-06-27 13:42:28 +0530

    Configure CORS for Netlify

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ const prescriptionRoutes = require("./routes/prescriptionRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://themediverse-khushi.netlify.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
